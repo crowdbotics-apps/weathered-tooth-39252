f8f4c41f39b43bc580d19071c0ed76fa199a9b0e
Crowdbotics <team@crowdbotics.com>
2023-03-07 00:06:40 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "weathered-tooth-39252",
+  url: "https://weathered-tooth-39252.botics.co",
+  api: "https://weathered-tooth-39252.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
